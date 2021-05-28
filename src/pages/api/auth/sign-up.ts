07836f8736a539c1c5b0aeca5c85c1e209a48399
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-05-28 19:15:07 +0530

    updates in handleSignUp api route

--- src/pages/api/auth/sign-up.ts
+++ src/pages/api/auth/sign-up.ts
@@ -2,32 +2,42 @@ import { handleHashPassword } from "../../../utils/auth";
 import { connectToDb } from "../../../utils/db";
 
 async function handleSignUp(req, res) {
-  const { email, password } = req.body;
+  switch (req.method) {
+    case "POST":
+      const { email, password } = req.body;
 
-  if (
-    !email ||
-    !email.includes("@") ||
-    !password ||
-    password.trim().length < 7
-  ) {
-    res.status(422).json({
-      message:
-        "Invalid Email Input / Password should be at least 7 characters long",
-    });
-    return;
+      if (
+        !email ||
+        !email.includes("@") ||
+        !password ||
+        password.trim().length < 7
+      ) {
+        res.status(422).json({
+          message:
+            "Invalid Email Input / Password should be at least 7 characters long",
+        });
+        return;
+      }
+
+      const client = await connectToDb();
+      const db = client.db();
+
+      const hashedPassword = await handleHashPassword(password);
+
+      await db.collection("users").insertOne({
+        email,
+        password: hashedPassword,
+      });
+
+      res.status(201).json({ message: "Created User!" });
+      break;
+
+    default:
+      res.status(400).json({
+        message: "Something went Wrong!",
+      });
+      break;
   }
-
-  const client = await connectToDb();
-  const db = client.db();
-
-  const hashedPassword = await handleHashPassword(password);
-
-  await db.collection("users").insertOne({
-    email,
-    password: hashedPassword,
-  });
-
-  res.status(201).json({ message: "Created User!" });
 }
 
 export default handleSignUp;
