6663d5dc8bd736bb01133a3d5d9983cd85de7e23
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-05-28 20:45:21 +0530

    Handle catch all NextAuth api route

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,6 +1,11 @@
-import { hash } from "bcryptjs";
+import { hash, compare } from "bcryptjs";
 
 export async function handleHashPassword(password) {
   const hashedPassword = await hash(password, 12);
   return hashedPassword;
+}
+
+export async function handleVerifyPassword(password, hashedPassword) {
+  const isValid = await compare(password, hashedPassword);
+  return isValid;
 }
