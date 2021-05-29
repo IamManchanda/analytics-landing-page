eaa1b96188a2ae05ecc82dcd23bb64c193f1a75c
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-05-29 23:32:55 +0530

    API Route: Adding the "Change Password" Logic

--- src/pages/api/user/change-password.ts
+++ src/pages/api/user/change-password.ts
@@ -1,4 +1,7 @@
 import { getSession } from "next-auth/client";
+import { handleHashPassword, handleVerifyPassword } from "../../../utils/auth";
+
+import { connectToDb } from "../../../utils/db";
 
 async function handleChangePassword(req, res) {
   switch (req.method) {
@@ -12,12 +15,67 @@ async function handleChangePassword(req, res) {
         return;
       }
 
+      const userEmail = session.user.email;
+      const { oldPassword, newPassword } = req.body;
+
+      const client = await connectToDb();
+
+      const usersCollection = await client.db().collection("users");
+
+      const user = await usersCollection.findOne({
+        email: userEmail,
+      });
+
+      if (!user) {
+        res.status(404).json({
+          message: "User not found!",
+        });
+
+        client.close();
+        return;
+      }
+
+      const currentPassword = user.password;
+
+      const arePasswordsEqual = await handleVerifyPassword(
+        oldPassword,
+        currentPassword,
+      );
+
+      if (!arePasswordsEqual) {
+        res.status(403).json({
+          message: "Invalid Password",
+        });
+
+        client.close();
+        return;
+      }
+
+      const hashedPassword = await handleHashPassword(newPassword);
+
+      await usersCollection.updateOne(
+        {
+          email: userEmail,
+        },
+        {
+          $set: {
+            password: hashedPassword,
+          },
+        },
+      );
+
+      res.status(200).json({
+        message: "Password updated!",
+      });
+
+      client.close();
       break;
 
     default:
       res.status(400).json({
         message: "Something went Wrong!",
       });
+
       break;
   }
 }
