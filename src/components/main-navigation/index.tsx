60044a983cabebecbfdc3e924b1b73c78f76a0a7
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-05-29 14:14:28 +0530

    Managing Active Session (On The Frontend)

--- src/components/main-navigation/index.tsx
+++ src/components/main-navigation/index.tsx
@@ -1,8 +1,11 @@
 import Link from "next/link";
+import { useSession } from "next-auth/client";
 
 import styles from "./styles.module.scss";
 
 function MainNavigation() {
+  const [session, loading] = useSession();
+
   return (
     <header className={styles.header}>
       <Link href="/">
@@ -12,20 +15,21 @@ function MainNavigation() {
       </Link>
       <nav>
         <ul>
-          <li>
-            <Link href="/">
-              <a>Home</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/auth">Login</Link>
-          </li>
-          <li>
-            <Link href="/profile">Profile</Link>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
+          {!session && !loading && (
+            <li>
+              <Link href="/auth">Login</Link>
+            </li>
+          )}
+          {session && (
+            <>
+              <li>
+                <Link href="/profile">Profile</Link>
+              </li>
+              <li>
+                <button>Logout</button>
+              </li>
+            </>
+          )}
         </ul>
       </nav>
     </header>
