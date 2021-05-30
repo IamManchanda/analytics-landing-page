c11d806515bd616b3eb35f78bec18cb695e6cbc9
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-05-30 21:44:16 +0530

    Remove unused code for this new project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,7 @@
-import { Provider } from "next-auth/client";
-
-import MainLayout from "../components/main-layout";
 import "../assets/styles/scss/globals.scss";
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <Provider session={pageProps.session}>
-      <MainLayout>
-        <Component {...pageProps} />
-      </MainLayout>
-    </Provider>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
--- src/utils/db.ts
+++
@@ -1,7 +0,0 @@
-import { MongoClient } from "mongodb";
-
-export async function connectToDb() {
-  const client = await MongoClient.connect(process.env.MONGODB_URI);
-
-  return client;
-}
