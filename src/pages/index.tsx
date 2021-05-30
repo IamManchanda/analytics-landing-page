69a01e12fef5ce1ec8de8cd7fc1ceca5880890b4
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-05-30 23:28:33 +0530

    Add Index Page

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+function PageIndex() {
+  return (
+    <>
+      <Head>
+        <title>Analytics Landing Page</title>
+      </Head>
+
+      <h1>Analytics Landing Page</h1>
+    </>
+  );
+}
+
+export default PageIndex;
