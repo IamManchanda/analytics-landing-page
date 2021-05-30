dcb074e8df39a53f19ad0338af9797e307863a3d
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-05-30 22:17:14 +0530

    Create Component: NewMeetupForm

---
+++ src/components/new-meetup-form/index.tsx
@@ -0,0 +1,62 @@
+import { useRef } from "react";
+
+import UiCard from "../ui-card";
+import styles from "./styles.module.scss";
+
+function NewMeetupForm(props) {
+  const titleInputRef = useRef<HTMLInputElement>();
+  const imageInputRef = useRef<HTMLInputElement>();
+  const addressInputRef = useRef<HTMLInputElement>();
+  const descriptionInputRef = useRef<HTMLTextAreaElement>();
+
+  function submitHandler(event) {
+    event.preventDefault();
+
+    const enteredTitle = titleInputRef.current.value;
+    const enteredImage = imageInputRef.current.value;
+    const enteredAddress = addressInputRef.current.value;
+    const enteredDescription = descriptionInputRef.current.value;
+
+    const meetupData = {
+      title: enteredTitle,
+      image: enteredImage,
+      address: enteredAddress,
+      description: enteredDescription,
+    };
+
+    props.onAddMeetup(meetupData);
+  }
+
+  return (
+    <UiCard>
+      <form className={styles.form} onSubmit={submitHandler}>
+        <div className={styles.control}>
+          <label htmlFor="title">Meetup Title</label>
+          <input type="text" required id="title" ref={titleInputRef} />
+        </div>
+        <div className={styles.control}>
+          <label htmlFor="image">Meetup Image</label>
+          <input type="url" required id="image" ref={imageInputRef} />
+        </div>
+        <div className={styles.control}>
+          <label htmlFor="address">Address</label>
+          <input type="text" required id="address" ref={addressInputRef} />
+        </div>
+        <div className={styles.control}>
+          <label htmlFor="description">Description</label>
+          <textarea
+            id="description"
+            required
+            rows={5}
+            ref={descriptionInputRef}
+          ></textarea>
+        </div>
+        <div className={styles.actions}>
+          <button>Add Meetup</button>
+        </div>
+      </form>
+    </UiCard>
+  );
+}
+
+export default NewMeetupForm;
