aae8085f45b3a981c9398128b54fb60bab95467d
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-05-31 23:30:43 +0530

    Code refactor: Put nav header in `_app` file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,14 @@
 import "tailwindcss/tailwind.css";
 
+import NavHeader from "../components/nav-header";
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div className="mx-auto max-w-75r">
+      <NavHeader />
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
 export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
-import NavHeader from "../components/nav-header";
+
 import JumboHero from "../components/jumbo-hero";
 import SetForFuture from "../components/set-for-future";
 import EverythingYouExpect from "../components/everything-you-expect";
@@ -14,21 +14,18 @@ function PageIndex() {
       <Head>
         <title>Analytics Landing Page Clone</title>
       </Head>
-      <div className="mx-auto max-w-75r">
-        <NavHeader />
-        <JumboHero />
-        <Image
-          src="https://pirsch.io/static/img/dashboard-dark.svg"
-          width={1200}
-          height={768}
-          layout="responsive"
-        />
-        <SetForFuture />
-        <EverythingYouExpect />
-        <ProductFeaturesList />
-        <ProductClients />
-        <CallToAction />
-      </div>
+      <JumboHero />
+      <Image
+        src="https://pirsch.io/static/img/dashboard-dark.svg"
+        width={1200}
+        height={768}
+        layout="responsive"
+      />
+      <SetForFuture />
+      <EverythingYouExpect />
+      <ProductFeaturesList />
+      <ProductClients />
+      <CallToAction />
     </>
   );
 }
