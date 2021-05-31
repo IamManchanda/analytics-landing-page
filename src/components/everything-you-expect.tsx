d2151949f56cb4aad10522d3ec0d46c77112482c
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-05-31 20:14:56 +0530

    More work at EverythingYouExpect component

--- src/components/everything-you-expect.tsx
+++ src/components/everything-you-expect.tsx
@@ -7,8 +7,8 @@ function EverythingYouExpect() {
       <h3 className="max-w-screen-md mx-auto text-6xl font-semibold leading-tight text-center">
         Everything You Expect, and More...
       </h3>
-      <div className="grid grid-cols-2 mt-20">
-        <div className="flex flex-col justify-center pr-14">
+      <div className="grid grid-cols-2 mt-20 gap-y-32 gap-x-24">
+        <div className="flex flex-col justify-center">
           <h4 className="text-3xl font-medium">
             Powerful Dashboard for Your Personal or Business Needs
           </h4>
@@ -35,6 +35,27 @@ function EverythingYouExpect() {
           height={663}
           layout="responsive"
         />
+        <Image
+          src="https://pirsch.io/static/img/access-dark.svg"
+          width={568}
+          height={663}
+          layout="responsive"
+        />
+        <div className="flex flex-col justify-center">
+          <h4 className="text-3xl font-medium">
+            Fine-grained Access Management Means Full Control
+          </h4>
+          <p className="mt-5 text-2xl leading-normal text-pir-gray-500">
+            Give your team members access to the dashboard by assigning each one
+            their own role, enabling them to see important data at a glance. If
+            you want, you can share your dashboard with the world using your
+            personal subdomain.
+          </p>
+          <p className="mt-5 text-2xl leading-normal text-pir-gray-500">
+            Generate unique access links for external partners, customers, or
+            clients. Created or deleted in seconds, so you are always on top.
+          </p>
+        </div>
       </div>
     </>
   );
