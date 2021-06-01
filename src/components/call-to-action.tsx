4bdff40829d24182d02d80aa8e4a538e4d89028f
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-06-01 20:19:40 +0530

    Responsive style changes: CallToAction

--- src/components/call-to-action.tsx
+++ src/components/call-to-action.tsx
@@ -1,9 +1,11 @@
 import PrimaryButton from "./primary-button";
 function CallToAction() {
   return (
-    <div className="mt-48 text-center bg-black py-28 rounded-xl">
-      <h2 className="text-6xl font-medium text-white">Ready to get Started?</h2>
-      <p className="mt-6 text-[1.375rem] text-pir-gray-500">
+    <div className="mt-24 text-center bg-black lg:mt-48 py-28 rounded-xl">
+      <h2 className="text-3xl font-medium text-white sm:text-5xl lg:text-6xl">
+        Ready to get Started?
+      </h2>
+      <p className="mt-6 text-lg lg:text-[1.375rem] text-pir-gray-500">
         Setting up your website only takes a few minutes.
       </p>
       <div className="mt-10">
