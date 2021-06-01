56d9a8189f731f089d48e49e574cc20158592732
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-06-01 20:16:58 +0530

    More responsive style changes: ProductClients

--- src/components/product-clients.tsx
+++ src/components/product-clients.tsx
@@ -14,14 +14,14 @@ function ProductClients() {
           <PrimaryButton href="/#wordpress" text="Add to Wordpress" />
         </div>
       </li>
-      <li className="flex items-center justify-center order-2 col-span-2 px-12 py-24 bg-black rounded-xl">
+      <li className="flex items-center justify-center order-2 col-span-2 px-12 py-24 bg-black lg:px-0 rounded-xl">
         <Image
           src="https://pirsch.io/static/img/wordpress.svg"
           width={384}
           height={87}
         />
       </li>
-      <li className="flex items-center justify-center order-4 col-span-2 px-12 py-24 bg-black lg:order-3 rounded-xl">
+      <li className="flex items-center justify-center order-4 col-span-2 px-12 py-24 bg-black lg:px-0 lg:order-3 rounded-xl">
         <Image
           src="https://pirsch.io/static/img/googlesearchconsole.svg"
           width={384}
